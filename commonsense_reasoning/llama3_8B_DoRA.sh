7393fa6c76a7bf74407a75c2370f30b4d78f3cb1
nbasyl <syliu1998@gmail.com>
2024-05-22 05:43:03 +0000

    add license

--- commonsense_reasoning/llama3_8B_DoRA.sh
+++ commonsense_reasoning/llama3_8B_DoRA.sh
@@ -1,4 +1,10 @@
-
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
 CUDA_VISIBLE_DEVICES=$4 python finetune.py \
     --base_model 'meta-llama/Meta-Llama-3-8B' \
     --data_path 'commonsense_170k.json' \
--- visual_instruction_tuning/peft/src/peft/mapping.py
+++ visual_instruction_tuning/peft/src/peft/mapping.py
@@ -1,4 +1,11 @@
 # coding=utf-8
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
 # Copyright 2023-present the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
